76fbf3eba00dfab8a298318267295fd326430845
Minsung Kim <idkms15@bu.edu>
2025-11-18 17:17:30 -0500

    Remove old service/resolver modules

--- backend/app/resolvers/users.py
+++
@@ -1,5 +0,0 @@
-from ..services.users import create_user as create_user_service
-
-
-def resolve_create_user(name: str, email: str) -> dict:
-    return create_user_service(name, email)
--- backend/app/resolvers/users/create_user_resolver.py
+++
@@ -1,5 +0,0 @@
-from ....services.users.create_user.create_user import execute as create_user_execute
-
-
-def resolve(name: str, email: str) -> dict:
-    return create_user_execute(name, email)
