6437db8c275ccc848814da5653fc6c9899d25f2b
Minsung Kim <idkms15@bu.edu>
2025-11-18 17:22:29 -0500

    Add pytest dependency and fix resolver import

--- backend/app/resolvers/users/resolver.py
+++ backend/app/resolvers/users/resolver.py
@@ -1,4 +1,4 @@
-from ....services.users.create_user.create_user import execute as create_user_execute
+from app.services.users.create_user.create_user import execute as create_user_execute
 
 
 def resolve_create_user(name: str, email: str) -> dict:
