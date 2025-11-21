8c33b487fe5f5865f83bc7bf080bb167085b1edd
Minsung Kim <88697052+MinsungKim0315@users.noreply.github.com>
2025-11-20 20:49:08 -0500

    Revert "Post page UI"

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -1,65 +1,12 @@
-"use client";
 import postText from "../data/postText.json";
 import { Barrio } from "next/font/google";
-import { useState } from 'react';
 
 const barrio = Barrio({
   weight: "400",
   subsets: ["latin"],
 });
 
-type Product = {
-  id: number;
-  title: string;
-  price: number;
-  condition: string;
-  status: string;             
-  created_at: string; 
-  sellerName: string;
-  sellerRating: number;
-  location: string | null;
-  description: string;
-  image: string;
-  category: string;
-};
-
-
-
 export default function PostPage() {
-
-  const [isPosted, setIsPosted] = useState(false);
-
-  function handleSubmit (e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-
-  const form = e.currentTarget;
-  const data = new FormData(form);
-
-  const title = data.get("title") as string;
-  const category = data.get("category") as string;
-  const price = Number(data.get("price"));
-  const condition = data.get("condition") as string;
-  const description = data.get("description") as string;
-  const imageFile = data.get("image") as File;
-
-  const product: Product = {
-    id: Date.now(),                       
-    title,
-    price,
-    condition,
-    status: "available",                   
-    created_at: new Date().toISOString(),
-    sellerName: "Unknown Seller",         
-    sellerRating: 5,
-    location: null,
-    description,
-    image: imageFile.name,                
-    category,
-  };
-  console.log("Constructed product:", product);
-  form.reset();
-  }
-
   return (
     <div className="flex flex-col min-h-screen items-center justify-start bg-[#EDEAE2] p-8">
 
@@ -68,12 +15,11 @@ export default function PostPage() {
         <h1 className={`${barrio.className} mt-3 text-5xl text-[#00013d]`}>Create a Post</h1>
 
 
-            <form onSubmit={handleSubmit} action="/api/post-item" method="POST" className="flex flex-col gap-4 p-6 border rounded w-full max-w-md text-[#00013d]">  
+            <form action="/api/post-item" method="POST" className="flex flex-col gap-4 p-6 border rounded w-full max-w-md text-[#00013d]">  
          
                 <label htmlFor="title">Item Name</label>
                  <input
                     type="text"
-                    placeholder="Item name"
                     id="title"
                     name="title"
                     required
@@ -123,7 +69,7 @@ export default function PostPage() {
 
                 <label htmlFor="description">Image</label>
                 <input
-                type="file" name="image" id="fileToUpload"
+                type="file" name="fileToUpload" id="fileToUpload"
                 required
                 ></input>
   
